3fd607ac80034672d3944bdf114719348769e210
Daniel Kindt <DanielTKindt@gmail.com>
2017-09-18 18:23:17 -0600

    some basic javascript layout.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,18 @@
-function courseProgress() {
-    var start = new Date(2017,9,5),
-        end = new Date(2017,12,21),
-        today = new Date();
-    return (Math.round(( ( today - start ) / ( end - start ) ) * 100) + "%");
+const questions = [
+    {
+        "question": "Which of these memes best describe how you feel watching BYU Football this year?",
+        "choices": {
+            "a": "img src"
+        }
+    }
+];
+
+function populateQuizForm() {
+
 }
+
+function submitAnswers() {
+
+}
+
+
