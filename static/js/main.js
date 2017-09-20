1df57068d0b59f680c72588807927d92efb19029
Daniel Kindt <DanielTKindt@gmail.com>
2017-09-20 13:48:18 -0600

    cleaned up populateQuiz function to be more readable (you should now be able to see what is happening).  Created two arrays in order to separate the questions and images.  By doing this, we could have the option to generate new images for a question, or vice versa. Removed the empty css file in the root directory.

--- static/js/main.js
+++ static/js/main.js
@@ -25,76 +25,6 @@ const questions = [
     }
 ];
 
-function getRandomQuestion() {
-    var randIdx = Math.floor(Math.random()*questions.length);
-    return questions[randIdx];
-}
-
-function populateQuiz() {
-
-    var answerDiv = document.getElementById("choice-images");
-    var questionDiv = document.getElementById("question");
-    var choicesDiv = document.getElementById("choices");
-
-    // Get a 'random' question
-    var q = getRandomQuestion();
-    console.log(q.question);
-    var question = document.createElement("p");
-    question.className = "text-center";
-    question.textContent = q.question;
-    questionDiv.appendChild(question);
-
-    // Iterate over the choices for the question.
-    var choices = q.choices;
-    for (var c in choices) {
-        if (choices.hasOwnProperty(c)) {
-
-            var newChoiceDiv = document.createElement("div");
-            newChoiceDiv.className = "radio";
-
-            var newChoiceLabel = document.createElement("label");
-            //newChoiceLabel.textContent = "Option " + c.toUpperCase();
-
-            newChoiceLabel.className = "radio";
-            var newChoiceInput = document.createElement("input");
-            newChoiceInput.type = "radio";
-            newChoiceInput.name = "optradio";
-
-            newChoiceLabel.appendChild(newChoiceInput);
-            newChoiceLabel.append("Option " + c.toUpperCase());
-
-            newChoiceDiv.appendChild(newChoiceLabel);
-            choicesDiv.appendChild(newChoiceDiv);
-
-            var newImg = document.createElement("img");
-            newImg.src = choices[c];
-            newImg.alt = c;
-
-            var newLink = document.createElement("a");
-            newLink.className = "thumbnail";
-            newLink.href = "#";
-            newLink.appendChild(newImg);
-
-            var newCaption = document.createElement("div");
-            var newH3 = document.createElement("h3");
-            newCaption.className = "caption";
-            newH3.className = "text-center";
-            newH3.textContent = c.toUpperCase();
-            newCaption.appendChild(newH3);
-            newLink.appendChild(newCaption);
-
-            var newDiv = document.createElement("div");
-            newDiv.className = "col-xs-4 col-md-4";
-            newDiv.appendChild(newLink);
-
-            answerDiv.appendChild(newDiv);
-
-            console.log(c + ":" + choices[c]);
-        }
-    }
-
-}
-
 function submitTheAnswers() {
 	var stringArray = [
 		"You are quite an emotional individual.",
@@ -107,7 +37,77 @@ function submitTheAnswers() {
 	document.getElementById("textback").innerHTML = theString;
 }
 
-function newQuestions() {
+// Just messing around
+const questionsV2 = [
+    "Which of these memes best describe how you feel watching BYU Football this year?",
+    "Which of these memes best describe how you felt when Donald Trump was elected as P.O.T.U.S.?",
+    "Which of these memes best describes how you are currently feeling about web programming?"
+];
+
+const images = [
+    "https://media.giphy.com/media/do8MAY6CkoXsc/giphy.gif", "https://media.giphy.com/media/3t7RAFhu75Wwg/giphy.gif", "https://media.giphy.com/media/NWg7M1VlT101W/giphy.gif",
+    "https://media.giphy.com/media/FEikw3bXVHdMk/giphy.gif", "https://media.giphy.com/media/3oz8xPPAvH1k8N6ZYA/giphy.gif", "https://media.giphy.com/media/l46Cv7xLYTFTYQza8/giphy.gif",
+    "https://media.giphy.com/media/d31vNo8crBC91xF6/giphy.gif", "https://media.giphy.com/media/CPskAi4C6WLHa/giphy.gif", "https://media.giphy.com/media/12NUbkX6p4xOO4/giphy.gif"
+];
+
+function randIdx(someList) {
+    return Math.floor(Math.random() * someList.length);
+}
+
+function newImages() {
+
+    var imgElements = document.getElementsByClassName("img-choice");
+    var imgElementSrc = [];
+    for (var i = 0; i < imgElements; i++) {
+        var result = imgElements[i];
+        console.log("img " + result);
+        imgElementSrc.push(imgElements[i].src);
+    }
+    console.log(imgElementSrc);
+
+    // get the src for the current images
+    // var oldSrc = [];
+    for (var c = 0; c < imgElements.length; c++) {
+        var oldSrc = imgElements[c].src;
+        do {
+            var newSrc = images[randIdx(images)];
+        } while(oldSrc == newSrc);
+        imgElements[c].src = newSrc;
+    }
+}
+
+function populateQuiz() {
+
+    // get a random question
+    var question = questionsV2[randIdx(questionsV2)];
+    document.getElementById("question").innerHTML = '<p class="text-center">' + question + '</p>';
+
+    // get three random images to use
+    // as the choices for above question.
+    var imgList = images.slice();
+    var ltrOptions = "ABC";
+    var numOptions = 3;
+
+    // we need to build up the string we will insert as innerHtml,
+    // otherwise we end up overwriting during the for loop.  not sure
+    // if this is actually cleaner or not.
+    var imgHtml = "";
+    var radioHtml = "";
+
+    for(var c = 0; c < numOptions; c++) {
+
+        // create a new image div
+        var idx = randIdx(imgList);
+        var imgSrc = imgList.splice(idx, 1);
+        imgHtml += '<div class="col-xs-4 col-md-4"><a href="#" class="thumbnail">' +
+            '<img src="' + imgSrc + '" class="img-choice"><div class="caption"><h3 class="text-center">' + ltrOptions[c] + '</h3></div></a></div>';
+
+        // create the answer radio buttons
+        radioHtml += '<div class="radio"><label class="radio"><input type="radio" name="optradio">Option ' + ltrOptions[c] +
+            '</label></div>';
+    }
+    document.getElementById("choice-images").innerHTML = imgHtml;
+    document.getElementById("choices").innerHTML = radioHtml;
 
 }
 
